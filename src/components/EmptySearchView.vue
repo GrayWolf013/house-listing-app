966a14d9227a21ec5e555af005f71daca72bad5c
Ahmed Besbes <ahm.besbes@gmail.com>
2023-02-21 16:48:35 +0100

    create EmptySearchView and update logic

---
+++ src/components/EmptySearchView.vue
@@ -0,0 +1,30 @@
+<template>
+      <div class="empty-container empty-state-message center">
+      <img src="@/assets/img_empty_houses@3x.png" height="100" />
+      <div>No results found.</div>
+      <div>Please try another keyword.</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmptySearchView',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<style lang="scss">
+
+.center {
+  margin: auto;
+  width: 60%;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
