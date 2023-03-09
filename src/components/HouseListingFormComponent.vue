81fcde271e897377a746fd5bafbe517071956a9b
Ahmed Besbes <ahm.besbes@gmail.com>
2023-03-09 04:28:22 +0100

    addcustom form validation

--- src/components/HouseListingFormComponent.vue
+++ src/components/HouseListingFormComponent.vue
@@ -1,29 +1,27 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="">
     <div class="input-field-title">Street name*</div>
     <input
       type="text"
       placeholder="Enter the street name"
       v-model="house.streetName"
       :class="{ error: !house.streetName && buttonClicked }"
-      required
     />
     <div class="flex">
       <div>
         <div class="input-field-title">House number*</div>
         <input
-          type="number"
+          type="text"
           placeholder="Enter the house number"
           v-model="house.houseNumber"
           :class="{ error: !house.houseNumber && buttonClicked }"
-          required
         />
       </div>
       <div class="spacer" />
       <div>
         <div class="input-field-title">Addition (optional)</div>
         <input
-          type="number"
+          type="text"
           placeholder="e.g. A"
           v-model="house.numberAddition"
         />
@@ -35,7 +33,6 @@
       placeholder="e.g. 1000 AA"
       v-model="house.zip"
       :class="{ error: !house.zip && buttonClicked }"
-      required
     />
     <div class="input-field-title">City*</div>
     <input
@@ -43,7 +40,6 @@
       placeholder="e.g. Utrecht"
       v-model="house.city"
       :class="{ error: !house.city && buttonClicked }"
-      required
     />
     <div class="input-field-title">Upload picture (PNG or JPG)*</div>
     <UploadImageComponent
@@ -53,27 +49,28 @@
 
     <div class="input-field-title">Price*</div>
     <input
-      type="number"
+      type="text"
       placeholder="e.g. €150.000"
       v-model="house.price"
       :class="{ error: !house.price && buttonClicked }"
-      required
     />
     <div class="flex">
       <div>
         <div class="input-field-title">Size*</div>
         <input
-          type="number"
+          type="text"
           placeholder="e.g. 60m²"
           v-model="house.size"
           :class="{ error: !house.size && buttonClicked }"
-          required
         />
       </div>
       <div class="spacer" />
       <div>
         <div class="input-field-title">Garage*</div>
-        <select v-model="house.hasGarage">
+        <select
+          v-model="house.hasGarage"
+          :class="{ error: !house.hasGarage && buttonClicked }"
+        >
           <option disabled value="">Please select an option</option>
           <option value="false">no</option>
           <option value="true">yes</option>
@@ -84,32 +81,29 @@
       <div>
         <div class="input-field-title">Bedrooms*</div>
         <input
-          type="number"
+          type="text"
           placeholder="Enter amount"
           v-model="house.bedrooms"
           :class="{ error: !house.bedrooms && buttonClicked }"
-          required
         />
       </div>
       <div class="spacer" />
       <div>
         <div class="input-field-title">Bathrooms*</div>
         <input
-          type="number"
+          type="text"
           placeholder="Enter amount"
           v-model="house.bathrooms"
           :class="{ error: !house.bathrooms && buttonClicked }"
-          required
         />
       </div>
     </div>
     <div class="input-field-title">Construction date*</div>
     <input
-      type="number"
+      type="text"
       placeholder="e.g. 1900"
       v-model="house.constructionYear"
       :class="{ error: !house.constructionYear && buttonClicked }"
-      required
     />
     <div class="input-field-title">Description*</div>
     <textarea
@@ -118,8 +112,10 @@
       placeholder="Enter description"
       v-model="house.description"
       :class="{ error: !house.description && buttonClicked }"
-      required
     />
+    <div class="error-label">
+      {{ errorMessage }}
+    </div>
     <div class="btn-container">
       <button class="btn" @click="submitButtonClicked">POST</button>
     </div>
@@ -143,6 +139,7 @@ export default {
     const store = useStore();
     const state = reactive({
       buttonClicked: false,
+      errorMessage: "",
     });
 
     const house = computed(() => {
@@ -159,16 +156,45 @@ export default {
       });
     }
 
-    function submitButtonClicked() {
-      state.buttonClicked = true;
+    function formInvalid() {
+      // Check if any required fields are empty
+      if (
+        !house.value.streetName ||
+        !house.value.houseNumber ||
+        !house.value.zip ||
+        !house.value.city ||
+        !house.value.price ||
+        !house.value.size ||
+        !house.value.bedrooms ||
+        !house.value.bathrooms ||
+        !house.value.constructionYear ||
+        !house.value.description ||
+        !house.value.hasGarage
+
+      ) {
+        state.errorMessage = "Please fill out all required fields.";
+        return true;
+      }
+
+      // Check if any fields have invalid types
+      if (
+        isNaN(house.value.houseNumber) ||
+        isNaN(house.value.price) ||
+        isNaN(house.value.size) ||
+        isNaN(house.value.bedrooms) ||
+        isNaN(house.value.bathrooms) ||
+        isNaN(house.value.constructionYear)
+      ) {
+        state.errorMessage = "Please enter valid numbers for numeric fields.";
+        return true;
+      }
+
+      return false;
     }
 
-    function submitForm() {
-      console.log("house.value");
-      console.log(house.value);
-      console.log("house.value");
-
-      context.emit("submitForm", house.value);
+    function submitButtonClicked() {
+      state.buttonClicked = true;
+      if (!formInvalid()) context.emit("submitForm", house.value);
     }
 
     return {
@@ -177,7 +203,6 @@ export default {
 
       selectImage,
       submitButtonClicked,
-      submitForm,
     };
   },
   components: { UploadImageComponent },
@@ -198,7 +223,8 @@ export default {
   margin: 50px 275px;
 }
 .error {
-  border: 1px solid red;
+  border: 1px solid #f00 !important;
+
   &::placeholder {
     color: red;
     opacity: 1; /* Firefox */
@@ -213,5 +239,9 @@ export default {
     /* Microsoft Edge */
     color: red;
   }
+}
+.error-label {
+  color: red;
+
 }
 </style>
