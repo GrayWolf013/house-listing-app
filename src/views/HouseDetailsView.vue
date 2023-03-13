292ee475f575a62182b349d8e2a0173fb4df7607
Ahmed Besbes <ahm.besbes@gmail.com>
2023-03-13 01:18:43 +0100

    update house listing for mobile

--- src/views/HouseDetailsView.vue
+++ src/views/HouseDetailsView.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="center">
-      <div class="back-button" @click="backButtonClicked">
-        <img
-          src="@/assets/ic_back_grey@3x.png"
-          alt="ic_back"
-          width="20"
-          height="20"
-        />
-        <div class="back-button-label">Back to overview</div>
-      </div>
-    <br />
-    <br />
+    <div class="back-button" @click="backButtonClicked">
+      <img
+        src="@/assets/ic_back_grey@3x.png"
+        alt="ic_back"
+        width="20"
+        height="20"
+      />
+      <div class="back-button-label desktop-only">Back to overview</div>
+    </div>
     <div class="details-body">
       <div class="details-card">
         <HouseDetailsCard :house="house" @toggleAlert="toggleAlert" />
@@ -69,7 +67,7 @@ export default {
     }
 
     function backButtonClicked() {
-      router.go(-1)
+      router.go(-1);
     }
 
     return {
@@ -77,7 +75,7 @@ export default {
       house,
       recomendedHouses,
       toggleAlert,
-      backButtonClicked
+      backButtonClicked,
     };
   },
   components: {
@@ -89,20 +87,43 @@ export default {
 
 <style lang="scss" scoped>
 .center {
-  margin: 40px 200px;
-
+  /* Styles for desktop devices */
+  @media screen and (min-width: 768px) {
+    margin: 40px 200px;
+    .back-button {
+      margin-bottom: 20px;
+    }
+  }
+  /* Styles for mobile devices */
+  @media screen and (max-width: 767px) {
+    margin-bottom: 80px;
+    position: relative;
+    .recomendations {
+      margin: 50px;
+    }
+    .back-button {
+      position: absolute;
+      z-index: 1;
+      margin: 50px 20px;
+      img {
+        filter: brightness(0) invert(1);
+      }
+    }
+  }
   .details-body {
-    display: flex;
+    @media screen and (min-width: 768px) {
+      display: flex;
+      .details-card {
+        flex: 1;
+        flex-basis: 60%;
+      }
+      .recomendations {
+        flex: 1;
+        margin-left: 40px;
+        flex-basis: 40%;
+      }
+    }
     width: 100%;
-  }
-  .details-card {
-    flex: 1;
-    flex-basis: 60%;
-  }
-  .recomendations {
-    flex: 1;
-    margin-left: 40px;
-    flex-basis: 40%;
   }
 }
 </style>
